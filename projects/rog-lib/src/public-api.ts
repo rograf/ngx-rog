dd5fc0bc10a89d8042f8a118894ab3b916d38b5b
Rafal Rogulski <rogulskirafal+git@gmail.com>
2020-10-04 20:07:18 +0200

    export table module

--- projects/rog-lib/src/public-api.ts
+++ projects/rog-lib/src/public-api.ts
@@ -5,3 +5,7 @@
 export * from './lib/rog-lib.service';
 export * from './lib/rog-lib.component';
 export * from './lib/rog-lib.module';
+
+export * from './lib/table/table.module';
+
+export * from './lib/_core/core.module';
