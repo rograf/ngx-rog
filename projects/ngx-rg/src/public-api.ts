faa5661badb9f519a35002b0c44a978d64a51b43
Rafal Rogulski <rogulskirafal+git@gmail.com>
2020-12-31 21:05:30 +0100

    fix for export

--- projects/ngx-rg/src/public-api.ts
+++ projects/ngx-rg/src/public-api.ts
@@ -14,6 +14,7 @@ export * from './lib/table/_models/RgTableOptions.interface';
 
 export * from './lib/_core/rg.module';
 export * from './lib/_core/rg-sort-by.pipe';
+export * from './lib/_core/rg-loader.directive';
 export * from './lib/_core/rg-template.directive';
 export * from './lib/_core/rg-filter.pipe';
 export * from './lib/_core/rg-model-change.directive';
